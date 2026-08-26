b32f98ec42c851eb12b5b4867cfd52becd9eb6c8
BhavyaAndavrapu <bhavyaandavarapu.24@kgpian.iitkgp.ac.in>
2026-08-26 22:20:41 +0530

    Update vercel rewrites for API subroutes

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'https://job-portal-seven-blond.vercel.app/'
+  baseURL: process.env.REACT_APP_API_URL || 'https://job-portal-seven-blond.vercel.app/api'
 });
 
 // Attach JWT token to every request if present
